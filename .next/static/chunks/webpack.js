27b38b1df32313d1477eb572a344e570696296e2
Mr.chu <chuhongkyu@gmail.com>
2022-12-24 23:41:55 +0900

    Add information

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0d24491056de0ebd"; }
+/******/ 		__webpack_require__.h = function() { return "bebce8bf748ec8f4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
