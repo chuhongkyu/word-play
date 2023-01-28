dee374471c835aeece37a1ce81a5a56d9cc54b2f
Mr.chu <chuhongkyu@gmail.com>
2023-01-28 21:57:59 +0900

    Add Data

--- utils/word-play.ts
+++ utils/word-play.ts
@@ -46,36 +46,30 @@ export const $data = [
     answer: "반입금지라서",
   },
   { level: 1, question: "피카츄가 담배를 피기 전에 하는 말?", answer: "피까" },
-
   { level: 1, question: "모든 사람을 일어나게 하는 숫자는?", answer: "다섯" },
-
   {
     level: 3,
     question: "사자가 항상 숙제를 안 하는 이유는?",
     answer: "밀림의 제왕이라서",
   },
-
   { level: 1, question: "자동차를 톡하고 치면?", answer: "카톡" },
-
   { level: 1, question: "다리미가 좋아하는 음식은?", answer: "피자" },
-
+  { level: 1, question: "빵이 시골로 간 까닭은?", answer: "소보로" },
   {
     level: 5,
     question: "겨울왕국의 엘사가 놀릴 때 하는 말은?",
     answer: "얼레리꼴레리",
-    why: "얼 Let it go 레리"
+    why: "얼 Let it go 레리",
   },
   { level: 1, question: "뽑으면 우는 식물은?", answer: "우엉" },
-
   { level: 1, question: "광부가 가장 많은 나라는?", answer: "케냐" },
-
   { level: 1, question: "사람이 울면?", answer: "흑인" },
-
   {
     level: 1,
     question: "오키나와에 가면 살이 찌는 이유는?",
     answer: "5끼 나오니까",
   },
+  { level: 1, question: "처음 만나는 소가 하는 말은?", answer: "반갑소" },
 
   { level: 1, question: "소나무가 삐지면?", answer: "칫솔" },
 
@@ -98,17 +92,15 @@ export const $data = [
   { level: 1, question: "세상에서 가장 가난한 왕은?", answer: "최저임금" },
 
   { level: 1, question: "칼이 정색하면?", answer: "검정색" },
-
   {
     level: 1,
     question: "가수 설운도가 옷을 벗는 순서가 어떻게 될까?",
     answer: "상하의",
   },
-
   { level: 1, question: "항상 미안해하는 연예인은?", answer: "지성" },
 
   { level: 1, question: "반성문을 영어로 하면?", answer: "글로벌" },
-  { level: 1, question: "세종대왕이 나온 고등학교는?", answer: "가갸거겨고교" },
+  { level: 2, question: "세종대왕이 나온 고등학교는?", answer: "가갸거겨고교" },
   { level: 1, question: "새우가 주인공인 드라마는?", answer: "대하드라마" },
   { level: 1, question: "화장실에서 금방 나온 사람은?", answer: "일본사람" },
   { level: 1, question: "회를 가장 잘 뜨는 곳은?", answer: "회전문" },
@@ -154,24 +146,21 @@ export const $data = [
 
   { level: 1, question: "딸기가 도망가면?", answer: "딸기쨈" },
   {
-    level: 1,
+    level: 3,
     question: "길 가다가 음식을 주운 것을 3자로?",
     answer: "푸드 득",
   },
-
   {
     level: 1,
     question: "옥수수가 시험을 보는 것을 4자로?",
     answer: "콘테스트",
   },
-
   { level: 1, question: "칼로 산을 두 개로 자르면?", answer: "두산베어스" },
-
   { level: 1, question: "물리치료를 받는 이유는?", answer: "병을 물리치료구" },
   { level: 1, question: "우유가 넘어지면?", answer: "아야" },
   { level: 5, question: "보내기 싫으면?", answer: "가위나 바위를 낸다" },
   { level: 1, question: "세상에서 가장 놀라운 거품은?", answer: "언빌리버블" },
-  { level: 1, question: "모내기 기계가 고장이 나면?", answer: "심기 불편" },
+  { level: 2, question: "모내기 기계가 고장이 나면?", answer: "심기 불편" },
   { level: 1, question: "간첩이 동물원에 가면 찾는 새는?", answer: "공작새" },
   { level: 1, question: "오래된 우유를 다른 말로?", answer: "연세우유" },
   {
@@ -206,4 +195,55 @@ export const $data = [
     question: "노크한 면접생이 합격한 이유는?",
     answer: "똑똑하니까",
   },
+  ///진 퀴즈
+  {
+    level: 1,
+    question: "물이 흩어지면 뭔지 아세요?",
+    answer: "해산물",
+  },
+  {
+    level: 1,
+    question: "오리를 생으로 먹으면 뭔지 아세요?",
+    answer: "회오리"
+  },
+  {
+    level: 1,
+    question: "소가 계단을 오르면?",
+    answer: "소오름"
+  },
+  {
+    level: 1,
+    question: "소가 전쟁을 하면 뭔지 아세요?",
+    answer: "워매워매애"
+  },
+  {
+    level: 4,
+    question: "라면이 프랑스가면 왜 싫어하는지 아십니까?",
+    answer: "다 불어 써서",
+  },
+  {
+    level: 4,
+    question: "신이 아이를 낳으면 뭔지 아십니까?",
+    answer: "갓 난 아이"
+  },
+  {
+    level: 4,
+    question: "고양이를 물로 만들면 뭔지 아십니까",
+    answer: "시무룩"
+  },
+  {
+    level: 4,
+    question: "여러분 클럽가지 마세요. 제가 여러분 OOO 이니까요",
+    answer: "팬클럽"
+  },
+  {
+    level: 4,
+    question: "손가락은 영어로 핑거잖아요. 그럼은 주먹은 뭐게요?",
+    answer: "오므린거"
+  },
+  {
+    level: 4,
+    question: "햄버거가 무슨 색인지 아니?",
+    answer: "버건디"
+  }
 ];
