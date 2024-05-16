ebadace4992f8f8ca9d77d0d78622688b21f7e21
Mr.chu <chuhongkyu@gmail.com>
2024-05-16 18:01:30 +0900

    Add Script Ads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -6,6 +7,11 @@ export default function Document() {
       <Head>
         <meta name="google-site-verification" content="mtYE_cYSNMHVF7KJPjly8GIJXgXfrbCG67BzEi3OY3c" />
         <meta name="naver-site-verification" content="dc8ef477fbdf278131748aa9fdf60b79da27dbd3" />
+        <Script 
+          id="adsbygoogle-init"
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1979490362657562" 
+          crossOrigin="anonymous"/>
       </Head>
       <body>
         <Main />
