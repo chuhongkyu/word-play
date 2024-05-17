34b66f8e88ffe5905b348d8c1110ae3d15ffe9c9
Mr.chu <chuhongkyu@gmail.com>
2024-05-17 17:04:36 +0900

    Add Custom Banner

---
+++ src/components/Ads/AdsList.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Adsense } from "@ctrl/react-adsense";
+
+export function AdsList() {
+  return (
+    <li>
+      <Adsense
+        client="ca-pub-1979490362657562"
+        slot="4012442095"
+        style={{ display: "block", width: "100%", overflow: "hidden"}}
+        layout="in-article"
+        layoutKey="-f9+5v+4m-d8+7b"
+        format="fluid"
+      />
+    </li>
+  );
+}
--- src/components/Home/ListContainer.tsx
+++ src/components/Home/ListContainer.tsx
@@ -4,7 +4,9 @@ import useFormattedDate from "@/utils/useFormatteDate"
 import Link from "next/link"
 import { IList } from "@/interface/listType";
 import { useQuiz } from "@/utils/useQuiz";
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
+import { AdsList } from "@/components/Ads/AdsList";
+import ArrowBtn from "@/components/icon/ArrowBtn";
 
 const ListContainer = ({data}:{ data: IList[]}) => {
     const { state, dispatch } = useQuiz()
@@ -21,32 +23,33 @@ const ListContainer = ({data}:{ data: IList[]}) => {
 
     return(
         <ul className="list">
-            {data?.map((el) =>{
+            
+            {data?.map((el, index) =>{
                 return(
-                    <li key={el._id + "KEY"}>
-                        <Link href={`/detail/${el._id}`}>
-                            <div className="list-wrapper">
-                                <h5 className="subtitle">{el.subtitle}</h5>
-                                <span className="keywords">
-                                    키워드 : {el?.keywords?.map((word, i) => <p key={i+"word"}>{word}
-                                    {i + 1 !== el?.keywords?.length && ',' }
-                                    </p>)}
-                                </span>
-                                <span className="date-time">{useFormattedDate(el.startDatetime)} {el?.testType === "list" ? <b className="mark">list</b> : <b className="mark">select</b>}</span>
-                            </div>
-                            {saveData[makeContent(el._id)]?.record.length > 0 &&
-                            <div className="list-status-bar">
-                                {saveData[makeContent(el._id)]?.record.filter((el)=> el == "success").length} / {saveData[makeContent(el._id)]?.record.length}
-                            </div> }
-                            <div className="arrow-btn">
-                                <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" clipRule="evenodd" d="M0.292893 11.7071C-0.0976311 11.3166 -0.0976312 10.6834 0.292893 10.2929L4.58579 6L0.292893 1.70711C-0.0976316 1.31658 -0.0976316 0.683417 0.292893 0.292893C0.683417 -0.0976315 1.31658 -0.0976315 1.70711 0.292893L6.70711 5.29289C7.09763 5.68342 7.09763 6.31658 6.70711 6.70711L1.70711 11.7071C1.31658 12.0976 0.683418 12.0976 0.292893 11.7071Z" fill="#45494E"/>
-                                </svg>
-                            </div>
-                        </Link>
-                    </li>
+                    <Fragment key={index + "LIST-KEY"}>
+                        {(index + 1) % 2 === 0 && <AdsList/>}
+                        <li className="li" key={el._id + "KEY"}>
+                            <Link href={`/detail/${el._id}`}>
+                                <div className="list-wrapper">
+                                    <h5 className="subtitle">{el.subtitle}</h5>
+                                    <span className="keywords">
+                                        키워드 : {el?.keywords?.map((word, i) => <p key={i+"word"}>{word}
+                                        {i + 1 !== el?.keywords?.length && ',' }
+                                        </p>)}
+                                    </span>
+                                    <span className="date-time">{useFormattedDate(el.startDatetime)} {el?.testType === "list" ? <b className="mark">list</b> : <b className="mark">select</b>}</span>
+                                </div>
+                                {saveData[makeContent(el._id)]?.record.length > 0 &&
+                                <div className="list-status-bar">
+                                    {saveData[makeContent(el._id)]?.record.filter((el)=> el == "success").length} / {saveData[makeContent(el._id)]?.record.length}
+                                </div> }
+                                <ArrowBtn/>
+                            </Link>
+                        </li>
+                    </Fragment>
                 )
             })}
+            
         </ul>
     )
 }
